9c5375a3e1afa8ed094e1c65066b9cc1f533d80a
LabMikey <78513560+LabMikey@users.noreply.github.com>
2021-07-09 12:30:08 +1200

    unfinsihed

--- 02_Tool_List.py
+++ 02_Tool_List.py
@@ -1,10 +1,10 @@
-budget = tool_list ={'Sea Salt Crakers': 2,
-            'Griffins Snax': 2.5,
-            'Pizza Shapes': 3.3,
-            'Arnotts Cheds': 3.99,
-            'Rosemary Wheat': 2,
-            'Orginal Rice Crakers': 1.65
-}
+tool_list_1 =[["Sea Salt Crakers", "2"]]
+tool_list_2 =[["Griffins Snax", "2.5"]]
+tool_list_3 =[["Pizza Shapes", "3.3"]]
+tool_list_4 =[["Arnotts Cheds", "3.99"]]
+tool_list_5 =[["Rosemary Wheat", "2"]]
+tool_list_6 =[["Orginal Rice Crakers", "1.65"]]
+
 
 
 
@@ -13,20 +13,21 @@ profit = 0
 
 
 
+budget = float(input("Budget: "))  # replace with functions
 
-budget = int(input("Budget: "))  # replace with functions
-
-if budget < 2:
-        tool_list = 'Sea Salt Crakers'
+if budget < 1:
+        print(tool_list_6)
+elif budget < 2:
+        print(tool_list_1)
 elif budget < 2.5:
-        tool_list = 2.5
+        print(tool_list_2)
 elif budget < 3.3:
-        tool_list = 3.3
+        print(tool_list_3)
 elif budget < 3.99:
-        tool_list = 3.99
+        print(tool_list_4)
 else:
         tool_list = 6.5
 
-print("{} : ${:.2f}".format(budget, tool_list))
+print("{} : {}".format(budget, tool_list_1, tool_list_2, tool_list_3, tool_list_4, tool_list_5, tool_list_6))
 
 print("Profit from tickets: ${:.2f}".format(profit))
