a6d6cf885cda59f33137f771f4bb6428616f4965
LabMikey <78513560+LabMikey@users.noreply.github.com>
2021-07-30 12:23:50 +1200

    finsihed weight checker, partial assessment base

---
+++ 00_Assessment_Base.py
@@ -0,0 +1,61 @@
+# function goes here
+import pandas
+import re
+
+def float_check(question, low_num, high_num):
+
+    error = "Please enter a whole number between {} " \
+            "and {}".format(low_num, high_num)
+
+    valid = False
+    while not valid:
+
+       # ask user for Budget and check it is valid
+        try:
+            response = float(input(question))
+
+            if low_num < response < high_num:
+                return response
+            else:
+                print(error)
+
+       # If an interger is not entered, display an error
+        except ValueError:
+            print(error)
+
+# main routine goes here
+budget = float_check("Budget: ", 0, 20)
+
+# Unit price conversion
+# Functions
+def print_menu():
+    print('Grams to Kilograms')
+
+def num_check(question, error, num_type):
+    valid = False
+
+    while not valid:
+
+        try:
+            response = num_type(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+def unit_cost():
+    gm = float(input('Enter a weight in Grams: '))
+    get_cost = float(input('How much does it cost? '))
+    kg = gm / 1000
+    unit_price = get_cost / kg
+    return unit_price
+
+print_menu()
+item = unit_cost()
+print(item)
+if budget <(item):
+    print('Sorry you have insufficient funds.' )
--- 03_Weight_Checker_v2.py
+++ 03_Weight_Checker_v2.py
@@ -1,3 +1,8 @@
+# Unit price conversion
+#Functions
+def print_menu():
+    print('Grams to Kilograms')
+
 def num_check(question, error, num_type):
     valid = False
 
@@ -14,5 +19,18 @@ def num_check(question, error, num_type):
         except ValueError:
             print(error)
 
-#Main Routine
-weight = num_check("Weight(Grams): ")
+
+def unit_cost():
+    gm = float(input('Enter a weight in Grams: '))
+    get_cost = float(input('How much does it cost? '))
+    kg = gm / 1000
+    unit_price = get_cost / kg
+    return unit_price
+    print('You entered {} Grams; ' 'Weight in KG: {} Kilograms'.format(gm, kg))
+
+
+print_menu()
+item = unit_cost()
+print(item)
+
+
